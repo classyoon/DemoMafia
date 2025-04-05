1193541a5307fe2f40e8b65436e83901cb527550
classyoon <96508736+classyoon@users.noreply.github.com>
2025-04-05 15:08:47 -0400

    Initial Commit

---
+++ DemoMafia/DemoMafiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoMafiaApp.swift
+//  DemoMafia
+//
+//  Created by Conner Yoon on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoMafiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoMafiaTests/DemoMafiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoMafiaTests.swift
+//  DemoMafiaTests
+//
+//  Created by Conner Yoon on 4/5/25.
+//
+
+import Testing
+@testable import DemoMafia
+
+struct DemoMafiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
